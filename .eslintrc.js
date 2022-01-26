ff192ea65b64b3592ce52953bb8a506e125fa84c
Atawodi Emmanuel <gmemmy22@gmail.com>
2022-01-26 18:51:42 +0100

    chore: finish setting up liniting and crash handler

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- App.tsx
+++ App.tsx
@@ -1,5 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Alert, StyleSheet, Text, View,
+} from 'react-native';
+import { setJSExceptionHandler, setNativeExceptionHandler } from 'react-native-exception-handler';
+
+const errorHandler = (e, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+      'Unexpected error occurred',
+      `Error: ${isFatal ? 'Fatal:' : ''} ${e.name} ${
+        e.message
+      }. Please close the app and start again!`,
+      [
+        {
+          text: 'Close',
+        },
+      ],
+    );
+  } else {
+    console.log(e); // So that we can see it in the ADB logs in case of Android if needed
+  }
+};
+
+setJSExceptionHandler(errorHandler, true);
+
+setNativeExceptionHandler((errorString) => {
+  console.log('setNativeExceptionHandler', errorString);
+});
 
 export default function App() {
   return (

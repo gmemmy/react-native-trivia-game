699111f33bea19da9c827d533729ebf8d7778a20
Atawodi Emmanuel <gmemmy22@gmail.com>
2022-01-26 23:21:58 +0100

    fix: disable swipe back action

--- src/navigator/navigator.tsx
+++ src/navigator/navigator.tsx
@@ -8,10 +8,14 @@ const Stack = createStackNavigator()
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{ headerShown: false, gestureEnabled: false }}>
         <Stack.Screen name="HomeScreen" component={Home} />
-        <Stack.Screen name="QuizScreen" component={Quiz} />
-        <Stack.Screen name="ResultsScreen" component={Results} />
+        <Stack.Screen name="QuizScreen" component={Quiz} options={{
+          gestureEnabled: false,
+        }} />
+        <Stack.Screen name="ResultsScreen" component={Results} options={{
+          gestureEnabled: false
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
